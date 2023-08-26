045f470844339e11a4591ff2bc44b7fd7bbe6ede
FeloMatta <mattafelo@gmail.com>
2023-08-26 17:55:21 +0200

    recuperato gli elementi principali della pagina

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+// OPERAZIONI INIZIALI
+
 // CONFIGURAZIONE DI ChatGPT
 const API_BASE_URL = 'https://api.openai.com/v1';
 const API_KEY = ''; // inserisci la tua chiave
 const GPT_MODEL = 'gpt-3.5-turbo';
+
+// Recuperiamo gli elementi principali della pagina
+const loader = document.querySelector('.loader');
+const genreButtons = document.querySelectorAll('.genre');
+const placeholder = document.querySelector('#placeholder');
+const stageTemplate = document.querySelector('#stage-template');
+const gameoverTemplate = document.querySelector('#gameover-template');
