6843e86114962406b004a402095c8918468cfb95
FeloMatta <mattafelo@gmail.com>
2023-08-26 18:22:04 +0200

    aggiunto classe game-started al body alla selezione del genere e preparato istruzioni per ChatGPT

--- main.js
+++ main.js
@@ -33,6 +33,31 @@ genreButtons.forEach(function(button){
         selectedGenre = button.dataset.genre;
 
         // 3. Avviamo la partita
-        // startGame();
+        startGame();
     });
 });
+
+// Funzione per avviare la partita
+function startGame() {
+    // 1. Inserisco la classe "game-started"
+    document.body.classList.add('game-started');
+    // 2. Preparo le istruzioni iniziali per chat GPT
+    completeChat.push({
+        role: `system`, // come si deve comportare ChatGPT
+        content: `Voglio che ti comporti come se fossi un classico gioco di avventura testuale. Io sarò il protagonista e giocatore principale. Non fare riferimento a te stesso. L\'ambientazione di questo gioco sarà a tema ${selectedGenre}. Ogni ambientazione ha una descrizione di 150 caratteri seguita da una array di 3 azioni possibili che il giocatore può compiere. Una di queste azioni è mortale e termina il gioco.
+
+
+        Non aggiungere mai altre spiegazioni. Non fare riferimento a te stesso. Le tue risposte sono solo in formato JSON come questo esempio:
+        
+        ###
+        
+        { 
+         "description":"descrizione ambientazione",
+          "actions":["azione 1", "azione 2", "azione 3"]
+        }
+         
+         ###`
+    });
+    // 3. Avvio il primo livello
+    setStage();
+}
